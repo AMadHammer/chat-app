4c9106f9a706f0c527e1f7e85e0b5b8908033e97
Ammar Ahmed <Ammar Ahmed>
2017-04-14 17:10:28 -0400

    got a chat client working

---
+++ index.js
@@ -0,0 +1,37 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/helloworld', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+    socket.broadcast.emit('new user connected');
+    console.log('a user connected');
+
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        socket.broadcast.emit('user disconnected');
+    });
+});
+
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+    
